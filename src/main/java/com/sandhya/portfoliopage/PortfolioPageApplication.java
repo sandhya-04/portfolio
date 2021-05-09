37c6d48399d747f0b54bf7bd1e0bb08145599d97
sandhya <sandy13097@gmail.com>
2021-05-09 14:12:17 +0530

    springboot-portfolio

---
+++ src/main/java/com/sandhya/portfoliopage/PortfolioPageApplication.java
@@ -0,0 +1,13 @@
+package com.sandhya.portfoliopage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioPageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sandhya/portfoliopage/PortfolioPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandhya.portfoliopage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
